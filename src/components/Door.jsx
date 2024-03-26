6635c5f0e4b63d3c7bd62ded9122ec5415eb38db
Austin Aluzzi <austinaluzzi@gmail.com>
2024-03-26 13:44:22 -0700

    Restyle door

--- src/components/Door.jsx
+++ src/components/Door.jsx
@@ -1,12 +1,16 @@
 function Door({content, opened, selected, select}) {
-    const BASE_STYLE = "h-[256px] w-[164px] flex items-end justify-center text-8xl shadow-lg" + (selected ? " ring-4 ring-sky-700" : "")
+    const BASE_STYLE = "h-[256px] w-[164px] px-4 py-6 flex text-8xl shadow-lg" + (selected ? " ring-4 ring-sky-700" : "")
     if (!opened) {
         return (
-            <div onClick={select} className={BASE_STYLE + " bg-slate-800"}></div>
+            <div onClick={select} className={BASE_STYLE + " items-center bg-slate-700"}>
+                <div className="bg-slate-500 rounded-full h-5 w-5 shadow-lg"></div>
+            </div>
         );
     } else {
         return (
-            <div className={BASE_STYLE + " bg-slate-700"}><div className="mb-4">{getEmojiFromContent(content)}</div></div>
+            <div className={BASE_STYLE + " items-end justify-center border-r-[16px] border-slate-700 bg-slate-800"}>
+                <div>{getEmojiFromContent(content)}</div>
+            </div>
         );
     }
 }
