fb805e3afec7cfbbc90787bbad7136049c18e45e
Austin Aluzzi <austinaluzzi@gmail.com>
2024-04-03 01:37:15 -0700

    Implement setting iteration count

--- src/components/Simulator.jsx
+++ src/components/Simulator.jsx
@@ -1,61 +1,82 @@
-import Button from "./Button";
+import Button from './Button';
 import { useState, useEffect } from 'react';
 
+const TOTAL_RUNTIME = 10000;
+
 function Simulator({ clickDoorElement, clickSwitchYesButton, clickSwitchNoButton, clickRestartButton }) {
-    const [switchDoors, setSwitchDoors] = useState(false);
-    const [iterationRunning, setIterationRunning] = useState(false);
-    const [iterationCount, setIterationCount] = useState(0);
-    const [iterationRuntime, setIterationRuntime] = useState(0);
+	const [switchDoors, setSwitchDoors] = useState(false);
+	const [iterationRunning, setIterationRunning] = useState(false);
+	const [iterationCount, setIterationCount] = useState(0);
+	const [iterationRuntime, setIterationRuntime] = useState(0);
 
-    const start = () => {
-        const totalRunningTime = 10000;
-        const iterations = 30;
-        setIterationCount(iterations);
-        setIterationRuntime(totalRunningTime / iterations);
+    const toggle = () => {
+        setIterationRuntime(TOTAL_RUNTIME / iterationCount);
+        setIterationRunning(running => !running);
     }
 
-    useEffect(() => {
-        if (iterationCount > 0) {
-            runIteration();
-        }
-        
-    }, [iterationCount]);
+	useEffect(() => {
+		if (iterationRunning && iterationCount > 0) {
+			runIteration();
+		} else {
+			setIterationRunning(false);
+		}
+	}, [iterationRunning, iterationCount]);
 
-    const runIteration = async () => {
-        const selectedDoorIndex = Math.floor(Math.random() * 3);
-        clickDoorElement(selectedDoorIndex);
-        await delay(iterationRuntime / 3);
-        if (switchDoors) {
-            clickSwitchYesButton();
-        } else {
-            clickSwitchNoButton();
-        }
-        await delay(iterationRuntime / 3);
-        clickRestartButton();
-        await delay(iterationRuntime / 3);
-        setIterationCount(count => count - 1);
-    }
+	const runIteration = async () => {
+		const selectedDoorIndex = Math.floor(Math.random() * 3);
+		clickDoorElement(selectedDoorIndex);
+		await delay(iterationRuntime / 3);
+		if (switchDoors) {
+			clickSwitchYesButton();
+		} else {
+			clickSwitchNoButton();
+		}
+		await delay(iterationRuntime / 3);
+		clickRestartButton();
+		await delay(iterationRuntime / 3);
+		setIterationCount((count) => count - 1);
+	};
 
-    const handleSwitchChange = () => {
-        setSwitchDoors(!switchDoors);
-    }
+	const handleSwitchChange = () => {
+		setSwitchDoors(!switchDoors);
+	};
 
-    return (
-        <div className="flex flex-col bg-slate-800 p-4 rounded-lg gap-2">
-            <h1 className="text-xl w-full">Simulator</h1>
-            <div className="flex flex-col bg-slate-700 rounded-md p-4 gap-2">
-                <div>
-                    <input className="m-1" type="checkbox" id="switch" name="switch" checked={switchDoors} onChange={handleSwitchChange} />
-                    <label htmlFor="switch">Switch Doors</label>
-                </div>
-                <Button onClick={start} text="Start" />
-            </div>
-        </div>
-    )
+	return (
+		<div className="flex flex-col bg-slate-800 p-4 rounded-lg gap-2">
+			<h1 className="text-xl w-full">Simulator</h1>
+			<div className="flex flex-col justify-start bg-slate-700 rounded-md p-4 gap-4">
+				<div className="flex flex-col text-left">
+					<label htmlFor="iterations">Iteration Count</label>
+					<input
+						className="bg-slate-600 p-2 rounded-md"
+						type="number"
+                        min="0"
+                        max="100000"
+                        id="iterations"
+						name="iterations"
+						value={iterationCount}
+						onChange={(e) => setIterationCount(e.target.value < 99999 ? e.target.value : 99999)}
+					/>
+				</div>
+				<div>
+					<input
+						className="m-1"
+						type="checkbox"
+						id="switch"
+						name="switch"
+						checked={switchDoors}
+						onChange={handleSwitchChange}
+					/>
+					<label htmlFor="switch">Switch Doors</label>
+				</div>
+				<Button onClick={toggle} text={!iterationRunning ? "Start" : "Stop"} />
+			</div>
+		</div>
+	);
 }
 
 function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export default Simulator;
